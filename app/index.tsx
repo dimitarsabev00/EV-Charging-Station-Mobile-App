59a31c31d9152f5a1281dac6c7dd76e3cb833029
Dimitar Sabev <dimitarsabev00@gmail.com>
2026-03-26 23:33:31 +0200

    Added Custom Fonts With Expo Font To Project

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,28 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function Index() {
+  // Use `useFonts` only if you can't use the config plugin.
+  const [loaded, error] = useFonts({
+    outfit: require("../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("../assets/fonts/Outfit-SemiBold.ttf"),
+    "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View
       style={{
@@ -9,7 +31,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome To EV Charging Station Mobile App</Text>
+      <Text style={{ fontSize: 20, fontFamily: "outfit" }}>
+        Welcome To EV Charging Station Mobile App
+      </Text>
     </View>
   );
 }
