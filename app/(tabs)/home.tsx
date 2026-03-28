c45e5a20544de158c118dfa1a81d8e2cd48c65a5
Dimitar Sabev <dimitarsabev00@gmail.com>
2026-03-28 17:17:05 +0200

    Added Header For HomeTabScreen

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/HomeTabScreen/Header";
 import { StyleSheet, View } from "react-native";
 import AppMapView from "../../components/HomeTabScreen/AppMapView";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
       <AppMapView />
     </View>
   );
@@ -12,5 +16,12 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerContainer: {
+    position: "absolute",
+    zIndex: 10,
+    padding: 60,
+    width: "100%",
+    paddingHorizontal: 20,
   },
 });
---
+++ components/HomeTabScreen/Header.tsx
@@ -0,0 +1,29 @@
+import { useUser } from "@clerk/clerk-expo";
+import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+export default function Header() {
+  const { user } = useUser();
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: user?.imageUrl }}
+        style={{ width: 45, height: 45, borderRadius: 99 }}
+      />
+      <Image
+        source={require("../../assets/images/logo.png")}
+        style={{ width: 200, height: 45, objectFit: "contain" }}
+      />
+      <FontAwesome name="filter" size={26} color="black" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
