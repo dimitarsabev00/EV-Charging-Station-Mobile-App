54a48c1a99b710235947e8fa38319e238e0996e6
Dimitar Sabev <dimitarsabev00@gmail.com>
2026-04-03 13:20:03 +0300

    Added Design And Logic For CurrentUser In ProfileTabScreen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,127 @@
-import { StyleSheet, Text, View } from "react-native";
+import Colors from "@/constants/Colors";
+import { useUser } from "@clerk/clerk-expo";
+import { useAuth } from "@clerk/clerk-react";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
 import React from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Linking,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
 
 const ProfileTab = () => {
+  const { user } = useUser();
+  const menu = [
+    {
+      id: 1,
+      name: "My Favorite",
+      icon: "heart-outline",
+    },
+    {
+      id: 2,
+      name: "Other Projects",
+      icon: "ios-file-tray-full-outline",
+    },
+    {
+      id: 3,
+      name: "Logout",
+      icon: "exit-outline",
+    },
+  ];
+
+  const { isLoaded, signOut } = useAuth();
+  const navigation = useNavigation();
   return (
     <View>
-      <Text>ProfileTab</Text>
+      <Text style={{ padding: 10, fontFamily: "outfit-medium", fontSize: 30 }}>
+        My
+        <Text style={{ color: Colors.PRIMARY }}> Profile</Text>
+      </Text>
+
+      {!user && (
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ActivityIndicator size={"large"} color={Colors.PRIMARY} />
+        </View>
+      )}
+      {/* User Profile Section  */}
+      <View style={{ alignItems: "center", marginTop: 10 }}>
+        <Image
+          source={{ uri: user.imageUrl }}
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 99,
+          }}
+        />
+        <Text
+          style={{
+            fontFamily: "outfit-medium",
+            fontSize: 25,
+            marginTop: 10,
+          }}
+        >
+          {user?.fullName}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 17,
+            marginTop: 5,
+            color: Colors.GRAY,
+          }}
+        >
+          {user?.primaryEmailAddress?.emailAddress}
+        </Text>
+      </View>
+      <FlatList
+        data={menu}
+        style={{
+          marginTop: 50,
+          marginHorizontal: 50,
+        }}
+        renderItem={({ item, index }) => (
+          <Pressable
+            onPress={() =>
+              item.id == 1
+                ? navigation.navigate("favorite")
+                : item.id == 3
+                  ? signOut()
+                  : Linking.openURL("https://tubeguruji-courses.vercel.app/")
+            }
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 20,
+              alignItems: "center",
+
+              margin: 10,
+            }}
+          >
+            <Ionicons name={item.icon} size={40} color={Colors.PRIMARY} />
+            <Text
+              style={{
+                fontSize: 20,
+                fontFamily: "outfit",
+              }}
+            >
+              {item.name}
+            </Text>
+          </Pressable>
+        )}
+      />
     </View>
   );
 };
 
 export default ProfileTab;
-
-const styles = StyleSheet.create({});
